f7d7402a4e8ca9a6c4d3023e6614000ed7db7a30
yashtyagi298 <tyagiyash097@gmail.com>
2025-11-07 17:47:54 +0530

    vercel config

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react() , tailwindcss()],
-  build:{
-    outDir:"dist"
-  },
+  base:process.env.VITE_BASE_PATH || "/HotelFortInn"
 });
